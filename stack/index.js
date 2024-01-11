15a6333f1b5850324648bb5852fcb03e00f772c5
NICASIA <manoj.kiranti@gmail.com>
2024-01-11 15:15:56 +0545

    stack added

---
+++ stack/index.js
@@ -0,0 +1,22 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(item) {
+        this.items.push(item);
+    }
+
+    pop() {
+        if (this.isEmpty()) return null;
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
