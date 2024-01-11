19ff47be41797e02fe9ff739595a6538f3b9ae35
NICASIA <manoj.kiranti@gmail.com>
2024-01-11 13:30:31 +0545

    queue added

---
+++ queue/index.js
@@ -0,0 +1,28 @@
+class Queue {
+    constructor(){
+        this.data = [];
+    }
+    enqueue(data){
+        this.data.push(data);
+    }
+    dequeue(){
+        if(this.isEmpty()) return null;
+        return this.data.pop();
+    }
+    nextItem() {
+        if(this.isEmpty()) return null;
+        return this.data[this.data.length -1]
+    }
+    size() {
+        return this.data.length;
+    }
+    isEmpty() {
+        return this.data.length === 0;
+    }
+}
+
+const queue = new Queue();
+queue.enqueue(1)
+queue.enqueue(1)
+
+console.log(queue.dequeue());
